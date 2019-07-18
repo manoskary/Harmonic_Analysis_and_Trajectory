b9b2937de769027df33c35bc508d9d03019a001b
Manos <manoskaristineos@gmail.com>
2019-07-18 16:59:55 +0200

    Binary Classification Testing and Results

---
+++ ChangeTonnetzSpace(Unfinished).py
@@ -0,0 +1,66 @@
+from FirstNotePosition import dictOfTonnetz, TonnetzToString
+from structural_functions import getKeyByValue
+
+
+def noteChange(key, value, newTonnetz):
+    x, y = value
+    newkey = getKeyByValue(newTonnetz, (x % 4, y % 3))
+    return newkey
+
+
+def chordDictToNewTonnetz(chordDict, newTonnetz):
+    newchordDict = dict()
+    for key, value in chordDict.items():
+        newkey = noteChange(key, value, newTonnetz)
+        newchordDict[newkey] = value
+    return newchordDict
+
+
+def trajectoryToNewTonnetz(trajectory, newTonnetz):
+    newlistOfChords = []
+    newTonnetz = dictOfTonnetz[newTonnetz]
+    for chordDict in trajectory.chordPositions:
+        newchordDict = chordDictToNewTonnetz(chordDict, newTonnetz)
+        newlistOfChords.append(list(newchordDict.keys()))
+    return(newlistOfChords)
+
+
+# TODO fixed coordinates to equilateral triangles to rotate but didn't fix
+# back coordinates.
+def coordRot(value, rotation):
+    x, y = value
+    # x = x + 0.5*y
+    # y = 0.87*y
+    if rotation == 90:
+        newvalue = (-y, x)
+    elif rotation == 180:
+        newvalue = (-x, -y)
+    elif rotation == 270:
+        newvalue = (y, -x)
+    else:
+        newvalue = (x, y)
+    return newvalue
+
+
+def noteRot(key, value, rotation, Tonnetz):
+    x, y = coordRot(value, rotation)
+    newkey = getKeyByValue(Tonnetz, (x % 3, y % 4))
+    return newkey
+
+
+def chordDictRot(chordDict, rotation, Tonnetz):
+    newchordDict = dict()
+    for key, value in chordDict.items():
+        newkey = noteRot(key, value, rotation, Tonnetz)
+        newchordDict[newkey] = value
+    return newchordDict
+
+
+def trajectoryRot(trajectory, rotation=180):
+    newlistOfChords = []
+    Tonnetz = TonnetzToString(trajectory.Tonnetz)
+    Tonnetz = dictOfTonnetz[Tonnetz]
+    for chordDict in trajectory.chordPositions:
+        newchordDict = chordDictRot(chordDict, rotation, Tonnetz)
+        newlistOfChords.append(list(newchordDict.keys()))
+    return(newlistOfChords)
---
+++ Comparison_Results/GraphObjects_new/Results on Binary Classification only with Trajectory Future.md
@@ -0,0 +1,42 @@
+# Results of Binary Classification
+
+### 18-07-2019
+
+#### Method : Trajectory with Future Calculations, all Graph Centralities + Tonnetz
+
+
+
+__Result Table__
+
+| Composer 1 | Composer 2 | Score  |
+|------------|------------|--------|
+| Jazz 		 | Bach 	  | 100 %    |
+| Beethoven	 | Bach 	  | 100 %    |
+| Mozart	 | Bach 	  | 91-100 % |
+| Schumann	 | Bach 	  | 97-100 % |
+| Palestrina | Bach 	  | 95-100 % |
+| Monteverdi | Bach 	  | 92-94 %  |
+| Chopin	 | Bach 	  | 97-100 % |
+| Beethoven  | Mozart 	  | 58-60 %  |
+| Schumann	 | Mozart 	  | 54 % 	 |
+| Chopin	 | Mozart 	  | 60-73 %  |
+| Palestrina | Mozart 	  | 91-94 %  |
+| Monteverdi | Mozart 	  | 94-97 %  |
+| Jazz		 | Mozart 	  | 71-82 %  |
+| Schumann	 | Beethoven  | 75-80 %  |
+| Chopin	 | Beethoven  | 78-81 %  |
+| Palestrina | Beethoven  | 98 % 	 |
+| Monteverdi | Beethoven  | 93-98 %  |
+| Jazz		 | Beethoven  | 83 % 	 |
+| Chopin	 | Schumann   | 59-64%	 |
+| Palestrina | Schumann   | 74-87%	 |
+| Monteverdi | Schumann   | 87-92%	 |
+| Jazz		 | Schumann   | 87-90%	 |
+| Palestrina | Chopin	  | 92-94%	 |
+| Monteverdi | Chopin	  | 92-94%	 |
+| Jazz		 | Chopin	  | 72-81%	 |
+| Palestrina | Jazz		  | 92-100%	 |
+| Monteverdi | Jazz		  | 97-100%	 |
+| Monteverdi | Palestrina | 81-89%	 |
+
+
